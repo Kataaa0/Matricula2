b10319e13bb2ce59b4fdda7a792e14c056fa4f78
Kataaa0 <129550235+Kataaa0@users.noreply.github.com>
2023-10-20 02:01:59 -0500

    Update Materia.java
    a

--- src/Materia.java
+++ src/Materia.java
@@ -1,16 +1,12 @@
 public class Materia {
-    private String nombre="";
-    private int codigo = 0;
-    private int numeroHoras = 0;
-    private Profesor Profesor1;
-    private Profesor Profesor2;
-    private Profesor Profesor3;
-    public Materia(){
+    private String nombre;
+    private int codigo;
+    private int numeroHoras;
+    private String Profesor1;
+    private String Profesor2;
+    private String Profesor3;
 
-    }
-
-
-    public Materia(String nombre, int codigo, int numeroHoras, Profesor profesor1, Profesor profesor2, Profesor profesor3) {
+    public Materia(String nombre, int codigo, int numeroHoras, String profesor1, String profesor2, String profesor3) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.numeroHoras = numeroHoras;
@@ -41,37 +37,58 @@ public class Materia {
         return numeroHoras;
     }
 
-    public Profesor getProfesor1() {
+    public String getProfesor1() {
         return Profesor1;
     }
 
-    public void setProfesor1(Profesor profesor1) {
+    public void setProfesor1(String profesor1) {
         Profesor1 = profesor1;
     }
 
-    public Profesor getProfesor2() {
+    public String getProfesor2() {
         return Profesor2;
     }
 
-    public void setProfesor2(Profesor profesor2) {
+    public void setProfesor2(String profesor2) {
         Profesor2 = profesor2;
     }
 
-    public Profesor getProfesor3() {
+    public String getProfesor3() {
         return Profesor3;
     }
 
-    public void setProfesor3(Profesor profesor3) {
+    public void setProfesor3(String profesor3) {
         Profesor3 = profesor3;
     }
 
     public void setNumeroHoras(int numeroHoras) {
         this.numeroHoras = numeroHoras;
+        System.out.println("021");
+        System.out.println("012");
+        System.out.println("003");
+
+    }
+    public void ingresarMaterias() {
+        this.nombre=nombre;
+
+        System.out.println("Fisica: ");
+        System.out.println("Calculo ");
+        System.out.println("Comunicacion: ");
+
+    }
 
 
-
-
-
+    public void ingresarProfesor() {
+        this.numeroHoras=numeroHoras;
+        //"Ingrese el nombre de la materia1: ");
+        System.out.println("2: ");
+        System.out.println("3 ");
+        System.out.println("2 ");
 
     }
 }
+
+
+
+    
+   
